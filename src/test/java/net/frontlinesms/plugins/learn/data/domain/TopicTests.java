a98a2a64f780e8bfdda1e219ce427eda64d809f3
Alex Anderson <alex@frontlinesms.com>
2011-12-23 18:16:43 +0000

    Added unit tests for topics.

---
+++ src/test/java/net/frontlinesms/plugins/learn/data/domain/TopicTests.java
@@ -0,0 +1,73 @@
+package net.frontlinesms.plugins.learn.domain;
+
+public class TopicTests extends net.frontlinesms.junit.BaseTestCase {
+	/** Instance of Topic under test */
+	Topic t;
+
+	public void setUp() { t = new Topic(); }
+
+	public void testIdInitiallyZero() {
+		// when
+		long id = t.getId();
+
+		// then
+		assertEquals(0, id);
+	}
+
+	public void testNameInitiallyNull() {
+		// when
+		String name = t.getName();
+
+		// then
+		assertNull(name);
+	}
+
+	public void testNameSetGet() {
+		// given
+		assertNull(t.getName());
+
+		// when
+		t.setName("Maths + English");
+
+		// then
+		assertEquals("Maths + English", t.getName());
+	}
+
+	public void testItemsInitiallyEmpty() {
+		// when
+		List<TopicItem> items = t.getItems();
+
+		// then
+		assertEquals(0, items.size());
+	}
+
+	public void testAddItemAppends() {
+		// given
+		TopicItem item1 = new FakeTopicItem();
+		TopicItem item2 = new FakeTopicItem();
+
+		// when
+		t.addItem(item1);
+
+		// then
+		assertEquals(1, t.getItems().size());
+		assertEquals(item1, t.getItems().get(0));
+
+		// when
+		t.addItem(item2);
+
+		// then
+		assertEquals(2, t.getItems().size());
+		assertEquals(item1, t.getItems.get(0));
+		assertEquals(item2, t.getItems.get(1));
+	}
+
+	public void testGetItemsIsUnmodifiable() {
+		// TODO find out what kind of exception this should throw and make this more specific.
+		try {
+			t.getItems().add(new FakeTopicItem());
+			fail("topic.items should be unmodifiable outside of Topic class");
+		} catch(RuntimeException ex) {
+			ex.printStackTrace(); // TODO remove this print once we know the exception type
+		}
+}
---
+++ src/test/java/net/frontlinesms/plugins/learn/data/repository/TopicDaoTests.java
@@ -0,0 +1,38 @@
+package net.frontlinesms.plugins.learn;
+
+import net.frontlinesms.junit.HibernateTestCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+class TopicDaoTests extends HibernateTestCase {
+	/** dao under test */
+	@Autowired TopicDao topicDao;
+
+	public void testSave() {
+		// given a topic exists in memory
+		Topic t = new Topic();
+		assertEquals(0, t.getId());
+		assertEquals(0, topicDao.count());
+
+		// when the topic is saved
+		topicDao.save(t);
+
+		// then there are topics in the database and the ID is set
+		assertTrue(t.getId() > 0);
+		assertEquals(1, topicDao.count());
+	}
+
+	public void testDelete() {
+		// given a topic is already saved
+		Topic t = new Topic();
+		assertEquals(0, topicDao.count());
+		topicDao.save(t);
+		assertEquals(1, topicDao.count());
+
+		// when the topic is deleted
+		topicDao.delete(t);
+
+		// then it is no longer in the database
+		assertEquals(0, topicDao.count());
+	}
+}
