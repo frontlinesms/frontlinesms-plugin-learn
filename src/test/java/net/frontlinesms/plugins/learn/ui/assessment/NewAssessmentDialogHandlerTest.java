8f908c3052ddce865d3133afdd9f7e00c7e00070
Alex Anderson <alex@frontlinesms.com>
2012-01-20 13:06:14 +0300

    Added tests for Assessment Schedule dialog.

--- src/test/java/net/frontlinesms/plugins/learn/ui/assessment/NewAssessmentDialogHandlerTest.java
+++ src/test/java/net/frontlinesms/plugins/learn/ui/assessment/NewAssessmentDialogHandlerTest.java
@@ -22,7 +22,48 @@ public class NewAssessmentDialogHandlerTest extends TopicChoosingDialogHandlerTe
 	public void testTitle() {
 		assertEquals("plugins.learn.assessment.new", $().getText());
 	}
-
+	
+	public void testGroupSelecterInitialisedEmpty() {
+		TODO("Test that the group selecter is initially empty");
+	}
+	
+	public void testMessagesInitialisedEmpty() {
+		assertEquals(0, $("lsMessages").getChildCount());
+	}
+	
+	public void testGroupInitialisation() {
+		// given
+		TODO("fill all fields except group chooser");
+		// then
+		assertFalse($("btSave").isEnabled());
+		
+		// when
+		TODO("set a valid group");
+		// then
+		assertTrue($("btSave").isEnabled());
+	}
+	
+	public void testMessageValidation() {
+		// given
+		TODO("fill all fields except messages");
+		
+		// then
+		assertFalse($("btSave").isEnabled());
+		
+		// when
+		TODO("add a valid message");
+		// then
+		assertTrue($("btSave").isEnabled());
+	}
+	
+	public void testNewMessageButton() {
+		// when
+		$("btNewMessage").click();
+		
+		// then
+		assertEquals("plugins.learn.assessment.message.new", $("dgEditAssessmentMessage").getText());
+	}
+	
 	@Override
 	public void testSaveButton() {
 		// given
